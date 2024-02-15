2745c76091642a2f283c305c74ed71550d0a0862
Virginie Dourson <virginiedourson@gmail.com>
2024-02-15 13:39:08 +0100

    Directives

---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appBorderCard]'
+})
+export class BorderCardDirective {
+  private initialColor : string = 'lightgrey';
+  private defaultColor : string = 'red';
+  private defaultHeight : number = 220;
+
+  constructor(private el:ElementRef) { 
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaultHeight);
+  }
+
+  @Input() borderColor : string;
+
+  @HostListener('mouseenter') onMouseEnter(){
+    this.setBorder(this.borderColor || this.defaultColor);
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.setBorder('lightgrey');
+  }
+
+  private setBorder(color: string){
+    this.el.nativeElement.style.border = 'solid 4px ' + color;
+  }
+
+  private setHeight(h: number){
+    this.el.nativeElement.style.height = `${h}px`;
+  }
+
+}
