2074a5576ea93b74a092b2bc0e432f26e56d23dc
Virginie Dourson <virginiedourson@gmail.com>
2024-02-26 11:51:40 +0100

    bootstrapApplication

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,44 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { AuthGuard } from './app/auth.guard';
+import { provideRouter, Routes } from '@angular/router';
+import { InMemoryDataService } from './app/in-memory-data.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        loadChildren: () => import("./app/pokemon/pokemon.routes").then(module => module.pokemonRoutes)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import("./app/login/login.component").then(module => module.LoginComponent)
+    },
+    {
+        path: '**',
+        loadComponent: () => import("./app/page-not-found/page-not-found.component").then(module => module.PageNotFoundComponent)
+    }
+];
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        importProvidersFrom(BrowserModule, FormsModule, HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })),
+        provideRouter(routes)
+    ]
+})
   .catch(err => console.error(err));
