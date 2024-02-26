3abe9457d98c5f0068f6fc9cb4f127c4b2f32208
Virginie Dourson <virginiedourson@gmail.com>
2024-02-26 11:28:25 +0100

    Remove unnecessary NgModule classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { PokemonModule } from './pokemon/pokemon.module';
+
 import { FormsModule } from '@angular/forms';
 import { InMemoryDataService } from './in-memory-data.service';
 import { LoginComponent } from './login/login.component';
@@ -31,15 +31,14 @@ const routes: Routes = [
 @NgModule({
     declarations: [AppComponent],
     imports: [
-        BrowserModule,
-        FormsModule,
-        HttpClientModule,
-        HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
-        PokemonModule,
-        RouterModule.forRoot(routes),
-        PageNotFoundComponent,
-        LoginComponent
-    ],
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
+    RouterModule.forRoot(routes),
+    PageNotFoundComponent,
+    LoginComponent
+],
     providers: [],
     bootstrap: [AppComponent]
 })
